3f944c207bbb3a2be35cb333d05568df7b5946a2
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-06 15:25:22 +0530

    added layout, added sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 function App() {
-  return <div className="">3D developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="bg-primary relative z-0">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        {/* About */}
+        <About />
+
+        {/* Experience */}
+        <Experience />
+
+        {/* Tech */}
+        <Tech />
+
+        {/* Works */}
+        <Works />
+
+        {/* Feedbacks */}
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
