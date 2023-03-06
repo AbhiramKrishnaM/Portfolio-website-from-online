ae828e521e9d32219db6817e2a62911258d13bc8
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-06 17:10:01 +0530

    finalised logo and added nav text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,21 @@ const Navbar = () => {
     <nav className="paddingX w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          className="flex items-center gap-2"
+          className="flex gap-4  items-center"
           to="/"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="Logo" className="w-24 h-24 object-contain" />
+          <img src={logo} alt="Logo" className="w-[10%] object-contain" />
+          <p className="text-white text-[26px]  font-normal cursor-pointer">
+            Abhiram{" "}
+            <span className="hidden sm:block text-[34px] font-semibold ">
+              {" "}
+              Krishna M.
+            </span>
+          </p>
         </Link>
       </div>
     </nav>
