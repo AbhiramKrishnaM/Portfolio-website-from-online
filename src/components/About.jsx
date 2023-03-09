09e965cf0df7118e0c24a529df6a9cceac945ccc
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-09 12:03:12 +0530

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import { services } from "../constants";
 
 import { fadeIn, textVariant } from "../utils/motion";
 
+import { SectionWrapper } from "../hoc";
+
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -56,4 +58,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
