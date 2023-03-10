8bc4475c3b4807b55f750013e96f360aa0ab4f81
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-10 15:31:53 +0530

    added work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return <div></div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className="sectionSubText">My work</p>
+        <h2 className="sectionHeadText">Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex ">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </motion.p>
+      </div>
+    </>
+  );
 };
 
-export default Works;
+export default SectionWrapper(Works, "works");
