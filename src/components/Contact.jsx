a3c5c8c01c398ac435e4db1910622c7f4bc07c67
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-03-12 14:52:36 +0530

    added earth, added email template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,15 +6,47 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// template_28v4pb2
+// service_w9f2hz6   service id
+// 0CZiJzwboFA5KgfKN public key
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({ name: "", email: "", message: "" });
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_w9f2hz6",
+        "template_28v4pb2",
+        {
+          from_name: form.name,
+          to_name: "Abhiram",
+          from_email: form.email,
+          to_email: "abhiramKrishna.8921@gmail.com",
+          message: form.message,
+        },
+        "0CZiJzwboFA5KgfKN"
+      )
+      .then(() => {
+        setLoading(false);
+        alert("Thank you! I will get back to you as soon as possible.");
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      });
+  };
   return (
     <div className="xl:mt-12 xl:flex-row   flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
